30da31ebf6906d3045ee8bc3244f32e7286414fd
Tala Alkhateeb <talaalkhateeb04@gmail.com>
2026-05-06 22:21:16 +0300

    switch to sqlite for testing

--- GPD_back/GPD_Back/test_settings.py
+++ GPD_back/GPD_Back/test_settings.py
@@ -3,9 +3,15 @@ import os
 from .settings import *  # noqa: F401,F403
 
 
-DATABASES["default"]["TEST"] = {
-    "NAME": os.environ.get("TEST_DB_NAME", "GPD_test"),
-}
+if os.environ.get("CI", "false").lower() == "true":
+    DATABASES["default"] = {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "test_db.sqlite3",
+    }
+else:
+    DATABASES["default"]["TEST"] = {
+        "NAME": os.environ.get("TEST_DB_NAME", "GPD_test"),
+    }
 
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 PASSWORD_HASHERS = [
